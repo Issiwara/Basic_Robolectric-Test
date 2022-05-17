879c5a78935075a09819cbffb9cb9e58070bea2f
ISSIWARA KAVINDA <issiwaraperera9912@gmail.com>
2022-05-17 18:37:30 +0530

    Test1

---
+++ app/src/test/java/com/example/robolectrictest/ExampleUnitTest.kt
@@ -0,0 +1,42 @@
+package com.example.robolectrictest
+import android.widget.TextView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+@RunWith(RobolectricTestRunner::class)
+class ExampleUnitTestTest {
+    @Test
+    fun test_text_on_activity() {
+        val activity = Robolectric.buildActivity(MainActivity::class.java).create().get()
+        val text : TextView = activity.findViewById<TextView>(R.id.main_activity_text)
+        assertEquals("Hello Issiwara!", text.text)
+    }
+
+    @Test
+    fun test_text_on_fragment(){
+        val fragment = FirstFragment()
+
+        val activity = Robolectric.buildActivity((
+                SecondActivity::class.java
+                ))
+            .create()
+            .start()
+            .resume()
+            .get()
+
+        val fragmentManager: FragmentManager = activity.supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(fragment, null)
+        fragmentTransaction.commit()
+
+        val textView : TextView = activity.findViewById<TextView>(R.id.tv_hello_fragment)
+        assertEquals("hello fragment", textView.text)
+    }
+}
